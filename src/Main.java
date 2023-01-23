6bd0314c014a565cc3e057dc628dd788170adb95
Alevtyna777 <119439779+Alevtyna777@users.noreply.github.com>
2023-01-23 22:14:21 +0200

    homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person personCredentialsAge = new Person();
+        Person personGender = new Person( "User2", "male");
+
+        Person personAllFields = new Person ("User 3", " ", 42);
+                Person personDefaultConstructor = new Person();
+
+   
+
     }
+
 }
